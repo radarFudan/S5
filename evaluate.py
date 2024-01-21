a926587faf4f3a27a815c3ec63754ec6107094c1
Malay <25528016+radarFudan@users.noreply.github.com>
2024-01-21 18:56:22 +0800

    LSTM on malay, running but not good, it does not show context extension, what's wrong?

--- evaluate.py
+++ evaluate.py
@@ -121,6 +121,7 @@ def evaluation(config, rngs, iteration, state, consecutive_loader=True, evaluate
     evaluation = []
 
     seq_len_list = [16 * 2 ** i for i in range(0,12)] # 16 to 32768
+
     for seq_len in seq_len_list:
         # Change the sequence length for evaluation
         # TODO, care about the out of memory issue. 
@@ -155,14 +156,21 @@ def evaluation(config, rngs, iteration, state, consecutive_loader=True, evaluate
         else:
             raise NotImplementedError(f"Hidden state initialization for {config.layer} not implemented")
 
-        rngs, avg_loss, avg_perplexity, avg_accuracy = validate(config, iteration, state, zero_hiddens, val_loader, rngs, val=1, seq_len=seq_len)
+        if config.dataset in ["wikitext103", "pile"]:        
+            p_eval_step = jax.pmap(partial(eval_step, config=config, vocab_size=config.vocab_size), axis_name='batch')
+        elif config.dataset in ["icl_synthetics"]:
+            pass
+        else:
+            raise NotImplementedError("Dataset not implemented")
+
+        rngs, avg_loss, avg_perplexity, avg_accuracy = validate(config, iteration, state, zero_hiddens, val_loader, rngs, val=1, seq_len=seq_len, p_eval_step=p_eval_step)
         evaluation.append((seq_len, 'val', avg_loss, avg_perplexity, avg_accuracy))
 
-        rngs, avg_loss, avg_perplexity, avg_accuracy = validate(config, iteration, state, zero_hiddens, test_loader, rngs, val=2, seq_len=seq_len)
+        rngs, avg_loss, avg_perplexity, avg_accuracy = validate(config, iteration, state, zero_hiddens, test_loader, rngs, val=2, seq_len=seq_len, p_eval_step=p_eval_step)
         evaluation.append((seq_len, 'test', avg_loss, avg_perplexity, avg_accuracy))
 
         if evaluate_train:
-            rngs, avg_loss, avg_perplexity, avg_accuracy = validate(config, iteration, state, zero_hiddens, train_loader, rngs, val=0, seq_len=seq_len)
+            rngs, avg_loss, avg_perplexity, avg_accuracy = validate(config, iteration, state, zero_hiddens, train_loader, rngs, val=0, seq_len=seq_len, p_eval_step=p_eval_step)
             evaluation.append((seq_len, 'train', avg_loss, avg_perplexity, avg_accuracy))
 
         # Write the validation losses to a CSV file
---
+++ logs/README.md
@@ -0,0 +1,14 @@
+LSTM
+    previous
+        T16, monotonically increasing, no context extension. 
+        T32, monotonically increasing, no context extension. 
+        T64, monotonically increasing, no context extension. 
+        T128, monotonically increasing, no context extension. 
+
+        What fuck? 
+    
+    zero
+        T16, The difference is very minor. 200 does not seem to be some reasonable values... 
+        T32,
+        T64,
+        T128, not running, get it running. 
